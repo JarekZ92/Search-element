b46d866fc37676ad12ffaa2507d2f306e5bf9f05
JarekZ92 <z.jarek92@gmail.com>
2018-11-18 13:46:09 +0100

    added values into array

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const container = document.querySelector('.main_container'),
+input = container.querySelector('input'),
+list,
+valuesContainer
 
+valuesContainer =[
+    'Kanada',
+    'USA',
+    'Polska',
+    'Włochy',
+    'Rosja',
+    'Niemcy'
+]
